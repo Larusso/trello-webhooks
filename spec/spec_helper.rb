7644685f21a0994d68bf6db5eedec4def0b52511
Manfred Endres <manfred.endres@tslarusso.de>
2015-03-14 14:30:24 +0100

    test different order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,8 @@ SimpleCov.start do
   add_filter 'spec/'
 end
 
-Coveralls.wear!
 CodeClimate::TestReporter.start
+Coveralls.wear!
 
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
 
